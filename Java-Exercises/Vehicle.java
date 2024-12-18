13a25fa08b0c4e0a267e7848d9ec0375c4933095
ranodeep.banerjee <ranodeep.banerjee@accenture.com>
2024-12-18 12:48:06 +0530

    Person class added

--- Java-Exercises/Vehicle.java
+++ Java-Exercises/Vehicle.java
@@ -17,9 +17,32 @@ class Car extends Vehicle{
         this.mileage = mileage;
     }
 
-    public static void main(String args[]){
-        Car car = new Car("red", 10, 100);
-        car.display();
+    @Override
+    void display() {
+        System.out.println("Car Details");
+        super.display();
+    }
+}
 
+class Bike extends Vehicle{
+    public Bike(String color, int wheels, int mileage){
+        this.color = color;
+        this.wheels = wheels;
+        this.mileage = mileage;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Bike Details");
+        super.display();
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Car car = new Car("red", 4, 70);
+        Bike bike = new Bike("green", 12, 100);
+        car.display();
+        bike.display();
     }
 }
